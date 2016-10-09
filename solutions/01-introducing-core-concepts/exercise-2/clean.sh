74fa5072a75c76f3297b23cc15a6c6cda1824af0
Joost van Wollingen <jvanwollingen@bol.com>
2016-10-09 19:16:28 +0200

    Removed solutions

--- solutions/01-introducing-core-concepts/exercise-2/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-srcPath=../../../labs/01-build-your-own
-targetDir="../exercise-1/target"
-
-# Remove while target directory,
-# to cleanup the compiled version of the application
-rm -rf "${targetDir}"
