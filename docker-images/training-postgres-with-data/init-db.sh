d6ba7148d0b101d1054b637d96e855f73b0c02b7
Ivo de Bruijn <idebruijn@bol.com>
2016-10-10 17:40:59 +0200

    typo
    Change-Id: Ibe870122bcfe79b9e1670e9dde23b06214480999

--- docker-images/training-postgres-with-data/init-db.sh
+++ docker-images/training-postgres-with-data/init-db.sh
@@ -5,5 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE testnet;
     \c testnet
     CREATE TABLE kv (key varchar(100) PRIMARY KEY, value varchar(100));
-    INSERT INTO kv VALUES ('provider','testnet on pg');
+    INSERT INTO kv VALUES ('provider','postgres database');
 EOSQL
