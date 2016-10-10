5e2aef6de870bf580fb7c601640179df4e547f68
Joost van Wollingen <jvanwollingen@bol.com>
2016-10-10 18:46:35 +0200

    More explanation with the assignments.

--- docker-images/training-postgres-with-data/init-db.sh
+++ docker-images/training-postgres-with-data/init-db.sh
@@ -5,5 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE testnet;
     \c testnet
     CREATE TABLE kv (key varchar(100) PRIMARY KEY, value varchar(100));
-    INSERT INTO kv VALUES ('provider','postgres database');
+    INSERT INTO kv VALUES ('provider','Now getting data from Postgres!');
 EOSQL
